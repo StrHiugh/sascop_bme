d503a94acb98ec75729da1fdf35d7f9398bae94b
Armando Perera <116786621+StrHiugh@users.noreply.github.com>
2025-11-05 16:38:17 -0600

    correccion en staticfiles

--- bme_subtec/settings.py
+++ bme_subtec/settings.py
@@ -114,7 +114,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'operaciones' / 'static']
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
